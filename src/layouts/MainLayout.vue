95be58ca4515746e91fae618593253cb51042e2c
Isuru Prabhath <unixisuru@gmail.com>
2024-08-29 22:40:03 +0530

    i

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Test
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Version 1.0.0</div>
       </q-toolbar>
     </q-header>
 
