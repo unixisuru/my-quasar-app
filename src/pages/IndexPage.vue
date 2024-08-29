a40fc0feebd69bd6bc7b35fb8b89b37a40dcfa5d
Isuru Prabhath <unixisuru@gmail.com>
2024-08-29 23:38:23 +0530

    init

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    
+    <div>
+      <h1>Welcome</h1>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
